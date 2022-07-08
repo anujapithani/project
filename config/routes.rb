266fe55510db7dc23d7f04e744dc166ff4bf615d
anujapithani <anujapithnai@gmail.com>
2022-07-08 11:03:29 +0530

    commitn

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,16 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
   get '/add_to_cart/:id' => 'carts#add_to_cart'
   get '/shopping' => 'shoppings#show'
   get '/lineitem' => 'line_items#show'
+  post '/orders/:id' => 'orders#create'
+  get '/orders' => 'orders#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :products
+  resources :addresses
 end
